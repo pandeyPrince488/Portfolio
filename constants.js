1ba6103774374560ae77859bfc188df48b69d0ef
pandeyPrince488 <92245086+pandeyPrince488@users.noreply.github.com>
2023-11-18 18:11:11 +0530

    Update constants.js

--- constants.js
+++ constants.js
@@ -114,7 +114,7 @@ export const PROJECTS = [
     blurImage: "/projects/blur/airbnb-blur.webp",
     description: "Airbnb UI clone using NextJS + Tailwind CSS 🛏️",
     gradient: ["#F14658", "#DC2537"],
-    url: "https://airbnb-web-clone.vercel.app/",
+    url: "https://govinds-bookbnb.netlify.app/",
     tech: ["react", "nextjs", "tailwindcss", "mapbox"],
   },
   {
